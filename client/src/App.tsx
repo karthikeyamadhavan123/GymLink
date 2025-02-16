9fd2c18f110af9ee7c8dde84e87628dd053ee67b
karthikeyamadhavan123 <karthikeyadonnipad2005@gmail.com>
2025-02-16 14:42:54 +0530

    frontend connecting to backend successful

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 
 import './App.css'
-import Login from './Login'
+
 
 function App() {
 
   return (
     <div className='text-rose-500 bg-white backdrop-blur-2xl'>
-      <Login/>
     </div>
   )
 }
