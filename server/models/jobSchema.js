389601c3f673dedf3fd0188843174da0390743b1
karthikeyamadhavan123 <karthikeyadonnipad2005@gmail.com>
2025-02-17 09:04:30 +0530

    notification-creation completed

--- server/models/jobSchema.js
+++ server/models/jobSchema.js
@@ -1,39 +1,61 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const jobSchema = new Schema({
+const jobSchema = new Schema(
+  {
     jobTitle: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     requirements: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     jobDetails: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     experienceRequired: {
-        type: Number,
-        required: true,
-        min: 0
+      type: Number,
+      required: true,
+      min: 0,
     },
     postedBy: {
-        type: Schema.Types.ObjectId,
-        ref: "Gym",
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "Gym",
+      required: true,
     },
-    salary:{
-        type:Number,
-        required:true,
-        min:0
+    salary: {
+      type: Number,
+      required: true,
+      min: 0,
     },
-    appliedJobApplicants:[{
+    appliedJobApplicants: [
+      {
         type: Schema.Types.ObjectId,
         ref: "JobApplication",
-        default:[]
-    }]
-}, { timestamps: true });
+        default: [],
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('JobPosting', jobSchema);
+jobSchema.post("save", async function (doc, next) {
+  try {
+    const Notification = mongoose.model("Notification");
+
+    // Create a notification when a job is posted
+    const notification = new Notification({
+      notificationMessage: `New job posted: ${doc.jobTitle}`,
+      postedBy: doc._id, // Associate the notification with the job
+    });
+
+    await notification.save();
+    next();
+  } catch (error) {
+    console.error("Error creating notification:", error);
+    next(error);
+  }
+});
+module.exports = mongoose.model("JobPosting", jobSchema);
---
+++ server/models/notificationSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+const notificationSchema = new Schema({
+    notificationMessage:{
+        type:String,
+        required:true
+    },
+    postedBy:{
+        type:Schema.Types.ObjectId,
+        ref:"JobPosting",
+        required:true
+    },
+},{timestamps:true})
+
+module.exports=mongoose.model('Notification',notificationSchema)
